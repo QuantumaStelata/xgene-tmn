88c911e26441067eb132e3c59a11e17642946c50
QuantumaStelata <quantumastelata@gmail.com>
2021-03-23 03:46:09 +0300

    Bug fixed

--- xgene/settings.py
+++ xgene/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     #     'PASSWORD': '',
     #     'HOST': 'ec2-54-155-226-153.eu-west-1.compute.amazonaws.com',
     #     'PORT': '5432',
-    }
+    # }
 
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
