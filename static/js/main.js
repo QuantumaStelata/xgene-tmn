855052155171d4c8cfc70a2bde19967d7c513e6c
QuantumaStelata <quantumastelata@gmail.com>
2021-04-12 01:45:33 +0300

    add sort in players table

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,25 @@ window.onload = function () {
       document.body.querySelector('.show').remove();
     }, 200);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const getSort = ({ target }) => {
+      const order = (target.dataset.order = -(target.dataset.order || -1));
+      const index = [...target.parentNode.cells].indexOf(target);
+      const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+      const comparator = (index, order) => (a, b) => order * collator.compare(
+          a.children[index].innerHTML,
+          b.children[index].innerHTML
+      );
+      
+      for(const tBody of target.closest('table').tBodies)
+          tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+
+      for(const cell of target.parentNode.cells)
+          cell.classList.toggle('sorted', cell === target);
+  };
+  
+  document.querySelectorAll('.players thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+  
+});
